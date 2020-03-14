25da4e6ab7851b1227083b4895a227ef9ab59ba6
kimjayluta <kimjayluta0721@gmail.com>
2020-03-14 20:52:59 +0800

    chore: Adds a mark compelete functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
-
-function Todo({ todo, index}){
-	return <div className="todo">{todo.text}</div>
+// Function that Prints the Todo list.
+function Todo({ todo, index, completeTodo, deleteTodo}){
+	return (
+		<div 
+			style={{textDecoration: todo.isCompleted 	? 'line-through' : ''}} 
+			className="todo"
+		>
+			{todo.text}
+			<div>
+				<button onClick={() => completeTodo(index)}>Complete</button>
+				<button onClick={() => deleteTodo(index)}>x</button>
+			</div>
+		</div>
+	);
 }
 
 function TodoForm({addTodo}) {
@@ -43,9 +54,17 @@ function App() {
 			isCompleted: false
 		}
 	]);
-
+	// Pag tig call ang AddTodo function...
 	const addTodo = text => {
+		// Ma e siset si Todo sa variable na newTodos...
 		const newTodos = [...todos, {text}];
+		// Tapus e cacall ang function na setTodos with parameter newTodos
+		setTodos(newTodos);
+	}
+
+	const completeTodo = index => {
+		const newTodos = [...todos];
+		newTodos[index].isCompleted = true;
 		setTodos(newTodos);
 	}
 
@@ -53,8 +72,9 @@ function App() {
 		<div className="app">
 			<div className="todo-list">
 				{todos.map((todo, index) => (
-					<Todo key={index} index={index} todo={todo} />
+					<Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
 				))}
+				{/* Pag nag enter ang user ning Todo e cacall ang function na AddTodo */}
 				<TodoForm addTodo={addTodo}/>
 			</div>
 		</div>
